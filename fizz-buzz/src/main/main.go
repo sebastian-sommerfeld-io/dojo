9d182ca9ced10bd97b17059d5684bf117f5496f5
Sebastian <sebastian@sommerfeld.io>
2022-12-21 13:45:43 +0000

    feat: Implement Kata "Fizz Buzz"  - #4

--- fizz-buzz/src/main/main.go
+++ fizz-buzz/src/main/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func answer() int {
-	return 6 * 9
+func isDivisible(i int, divider int) bool {
+	return (i % divider) == 0
+}
+
+func fizzbuzz(i int) string {
+	if isDivisible(i, 3) && isDivisible(i, 5) {
+		return "fizzbuzz"
+	} else if isDivisible(i, 3) {
+		return "fizz"
+	} else if isDivisible(i, 5) {
+		return "buzz"
+	}
+	return strconv.Itoa(i)
 }
 
 func main() {
-	fmt.Println(answer())
+	for i := 1; i <= 100; i++ {
+		fmt.Println(fizzbuzz(i))
+	}
 }
--- fizz-buzz/src/main/main_test.go
+++ fizz-buzz/src/main/main_test.go
@@ -5,6 +5,37 @@ import (
 	"testing"
 )
 
-func Test_life_the_universe_and_everything(t *testing.T) {
-	assert.Equal(t, 42, answer())
+func Test_should_return_string_for_integer(t *testing.T) {
+	assert.Equal(t, "2", fizzbuzz(2))
+}
+
+func Test_should_return_fizz_for_three(t *testing.T) {
+	assert.Equal(t, "fizz", fizzbuzz(3))
+}
+
+func Test_should_return_buzz_for_five(t *testing.T) {
+	assert.Equal(t, "buzz", fizzbuzz(20))
+}
+
+func Test_should_return_fizz_if_dividable_by_three(t *testing.T) {
+	assert.Equal(t, true, isDivisible(9, 3))
+	assert.Equal(t, false, isDivisible(8, 3))
+	assert.Equal(t, "fizz", fizzbuzz(9))
+}
+
+func Test_should_return_fizz_if_dividable_by_five(t *testing.T) {
+	assert.Equal(t, true, isDivisible(20, 5))
+	assert.Equal(t, false, isDivisible(8, 5))
+	assert.Equal(t, "buzz", fizzbuzz(20))
+}
+
+func Test_should_return_fizz_if_dividable_by_three_and_five(t *testing.T) {
+	assert.Equal(t, true, isDivisible(15, 3))
+	assert.Equal(t, true, isDivisible(15, 5))
+	assert.Equal(t, false, isDivisible(8, 3))
+	assert.Equal(t, false, isDivisible(8, 5))
+	assert.Equal(t, "fizzbuzz", fizzbuzz(15))
+}
+
+func Test_should_contain_100_elements(t *testing.T) {
 }
