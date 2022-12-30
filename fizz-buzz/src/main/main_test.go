dd1671371c4f3299dbb2c9a8d8abac31f9f400d8
sebastian <sebastian@sommerfeld.io>
2022-12-30 21:16:38 +0100

    refactor: updated method names

--- fizz-buzz/src/main/main_test.go
+++ fizz-buzz/src/main/main_test.go
@@ -17,19 +17,19 @@ func Test_should_return_buzz_for_five(t *testing.T) {
 	assert.Equal(t, "buzz", fizzbuzz(20))
 }
 
-func Test_should_return_fizz_if_dividable_by_three(t *testing.T) {
+func Test_should_return_fizz_if_divisible_by_three(t *testing.T) {
 	assert.Equal(t, true, isDivisible(9, 3))
 	assert.Equal(t, false, isDivisible(8, 3))
 	assert.Equal(t, "fizz", fizzbuzz(9))
 }
 
-func Test_should_return_fizz_if_dividable_by_five(t *testing.T) {
+func Test_should_return_fizz_if_divisible_by_five(t *testing.T) {
 	assert.Equal(t, true, isDivisible(20, 5))
 	assert.Equal(t, false, isDivisible(8, 5))
 	assert.Equal(t, "buzz", fizzbuzz(20))
 }
 
-func Test_should_return_fizz_if_dividable_by_three_and_five(t *testing.T) {
+func Test_should_return_fizz_if_divisible_by_three_and_five(t *testing.T) {
 	assert.Equal(t, true, isDivisible(15, 3))
 	assert.Equal(t, true, isDivisible(15, 5))
 	assert.Equal(t, false, isDivisible(8, 3))
