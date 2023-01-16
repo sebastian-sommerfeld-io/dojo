f97ed201ee77bbbb06190034c42d3c81cae27979
sebastian <sebastian@sommerfeld.io>
2023-01-16 17:05:33 +0100

    renamed functions

--- fizz-buzz/src/main/main_test.go
+++ fizz-buzz/src/main/main_test.go
@@ -5,31 +5,31 @@ import (
 	"testing"
 )
 
-func Test_should_return_string_for_integer(t *testing.T) {
+func TestShouldReturnStringForInteger(t *testing.T) {
 	assert.Equal(t, "2", fizzbuzz(2))
 }
 
-func Test_should_return_fizz_for_three(t *testing.T) {
+func TestShouldReturnFizzForThree(t *testing.T) {
 	assert.Equal(t, "fizz", fizzbuzz(3))
 }
 
-func Test_should_return_buzz_for_five(t *testing.T) {
+func TestShouldReturnBuzzForFive(t *testing.T) {
 	assert.Equal(t, "buzz", fizzbuzz(20))
 }
 
-func Test_should_return_fizz_if_divisible_by_three(t *testing.T) {
+func TestShouldReturnFizzIfDivisibleByThree(t *testing.T) {
 	assert.Equal(t, true, isDivisible(9, 3))
 	assert.Equal(t, false, isDivisible(8, 3))
 	assert.Equal(t, "fizz", fizzbuzz(9))
 }
 
-func Test_should_return_fizz_if_divisible_by_five(t *testing.T) {
+func TestShouldReturnFizzIfDivisibleByFive(t *testing.T) {
 	assert.Equal(t, true, isDivisible(20, 5))
 	assert.Equal(t, false, isDivisible(8, 5))
 	assert.Equal(t, "buzz", fizzbuzz(20))
 }
 
-func Test_should_return_fizz_if_divisible_by_three_and_five(t *testing.T) {
+func TestShouldReturnFizzIfDivisibleByThree_andFive(t *testing.T) {
 	assert.Equal(t, true, isDivisible(15, 3))
 	assert.Equal(t, true, isDivisible(15, 5))
 	assert.Equal(t, false, isDivisible(8, 3))
